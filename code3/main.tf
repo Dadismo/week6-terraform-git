3d57f9ce9f39ffeed23094339daeae491f8736bc
Franck SORO <francksoro@Air-de-Franck.lan>
2024-02-27 20:11:54 +0100

    region updated for version and main in code3

--- code3/main.tf
+++ code3/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lightsail_instance" "my_server1" {
   name              = "week_5"
-  availability_zone = "eu-west-3a"
+  availability_zone = "us-east-2a"
   blueprint_id      = "centos_7_2009_01"
   bundle_id         = "micro_1_0"
   user_data         = "sudo su - yum install -y httpd && sudo su - systemctl start httpd && sudo su - systemctl enable httpd && echo '<h1>Deployed via Terraform</h1>' | sudo su - tee /var/www/html/index.html"
