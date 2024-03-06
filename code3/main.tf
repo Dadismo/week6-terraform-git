0329e4bfe52632ae19cda44e84abd57f950ea157
Franck SORO <francksoro@Air-de-Franck.lan>
2024-03-06 02:53:24 +0100

    changes made for ec2 instance

--- code3/main.tf
+++ code3/main.tf
@@ -1,7 +1,55 @@
-resource "aws_lightsail_instance" "my_server1" {
-  name              = "week_5"
-  availability_zone = "us-east-2a"
-  blueprint_id      = "centos_7_2009_01"
-  bundle_id         = "micro_1_0"
-  user_data         = "sudo su - yum install -y httpd && sudo su - systemctl start httpd && sudo su - systemctl enable httpd && echo '<h1>Deployed via Terraform</h1>' | sudo su - tee /var/www/html/index.html"
+ # Create Web Security Group
+resource "aws_security_group" "web-sg" {
+  name        = "docker-Web-SG"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  ingress {
+      description = "ingress port "
+      #from_port   = ingress.value
+      from_port   = 8000
+      to_port     = 8100
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    
+  }
+egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "docker-Web-SG"
+  }
 }
+
+#data for amazon linux
+
+data "aws_ami" "amazon-2" {
+    most_recent = true
+  
+    filter {
+      name = "name"
+      values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+    }
+    owners = ["amazon"]
+  }
+ 
+ # Create a EC2 instance
+resource "aws_instance" " EC2" {
+  ami              = data.aws_ami.amazon-2.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.web-sg.id ]
+  user_data         = file("script.sh")
+
+tags = {
+    Name = "docker instance"
+  }
+  provisioner "local-exec" {
+    command = "sleep 300"
+  }
+}
+
+ 
---
+++ output_complement.tf
@@ -0,0 +1,3 @@
+output "instance_IP" {
+  value = aws_lightsail_instance.my_server1.availability_zone
+}
